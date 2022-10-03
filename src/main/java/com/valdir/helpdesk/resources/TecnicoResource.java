39dbd85b753ab426baa0f0d56d622a90e29e6005
RodrigoAVeloso <RodrigoAVeloso@hotmail.com>
2022-10-03 12:30:47 -0300

    Endpoind find all para tecnico

--- src/main/java/com/valdir/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/valdir/helpdesk/resources/TecnicoResource.java
@@ -1,5 +1,8 @@
 package com.valdir.helpdesk.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,4 +26,12 @@ public class TecnicoResource {
 		Tecnico obj = service.findById(id);
 		return ResponseEntity.ok().body(new TecnicoDto(obj));
 	}
+	
+	@GetMapping()
+	public ResponseEntity<List<TecnicoDto>> findAll(){
+		List<Tecnico> list = service.findAll();
+		List<TecnicoDto> listDto = list.stream().map(obj -> new TecnicoDto(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
 }
+
