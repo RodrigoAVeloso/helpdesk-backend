a1abe4d0f499135d3b791ccdc7a4c66def0a61d7
RodrigoAVeloso <RodrigoAVeloso@hotmail.com>
2022-10-04 15:46:56 -0300

    Exceção para CPF e Email já cadastrado

--- src/main/java/com/valdir/helpdesk/repositories/TecnicoRepository.java
+++ src/main/java/com/valdir/helpdesk/repositories/TecnicoRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.valdir.helpdesk.domain.Tecnico;
 
 public interface TecnicoRepository extends JpaRepository<Tecnico, Integer> {
-
 }
