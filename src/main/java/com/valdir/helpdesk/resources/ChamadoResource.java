95dc6b75fbf1cf4e08f6f6bfa2389919667e0d39
RodrigoAVeloso <RodrigoAVeloso@hotmail.com>
2022-10-05 16:09:56 -0300

    FindAll para chamados criado

--- src/main/java/com/valdir/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/valdir/helpdesk/resources/ChamadoResource.java
@@ -1,5 +1,8 @@
 package com.valdir.helpdesk.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,5 +25,12 @@ public class ChamadoResource {
 	public ResponseEntity<ChamadoDto> findById(@PathVariable Integer id){
 		Chamado obj = service.findById(id);
 		return ResponseEntity.ok().body(new ChamadoDto(obj));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<ChamadoDto>> findAll(){
+		List<Chamado> list = service.findAll();
+		List<ChamadoDto> listDto = list.stream().map(obj -> new ChamadoDto(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
